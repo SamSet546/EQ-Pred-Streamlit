ded7ab01462a7af3e45d97da5075102d7d95ef1e
SamSet546 <139157616+SamSet546@users.noreply.github.com>
2024-08-05 18:48:06 -0700

    Update 1_📈_Regression_Systems.py

--- pages/1_📈_Regression_Systems.py
+++ pages/1_📈_Regression_Systems.py
@@ -433,7 +433,7 @@ def prog_GS(X, y, progress_bar):
             if current_fit % (total_fits // 10) == 0:
                 progress = current_fit / total_fits
                 progress_bar.progress(progress)
-                st.session_state.text_status = f"Processing fits {current_fit}/{total_fits}
+                #st.session_state.text_status = f"Processing fits {current_fit}/{total_fits}
                 text_status.text(f"Processing fits {current_fit}/{total_fits}""
             #time.sleep(0.1)  # Simulate delay
     
